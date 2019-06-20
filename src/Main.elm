5ce9abc7848728633b2b801f20bd99f7ccf5a73f
Leif Battermann <leif.battermann@gmail.com>
2019-06-20 11:22:00 +0200

    subtraction

--- src/Main.elm
+++ src/Main.elm
@@ -9,6 +9,7 @@ import Bootstrap.Utilities.Spacing as Spacing
 import Browser
 import Html exposing (Html)
 import Html.Attributes
+import List.Nonempty as Nel exposing (Nonempty)
 import Random
 import Random.List
 
@@ -18,7 +19,8 @@ import Random.List
 
 
 type Operation
-    = Add
+    = Addition
+    | Subtraction
 
 
 type Answer
@@ -27,26 +29,32 @@ type Answer
     | NotAnswered
 
 
+type alias Term =
+    { operation : Operation
+    , first : Int
+    , second : Int
+    , result : Int
+    }
+
+
 type alias Model =
     { upper : Int
-    , operation : Operation
-    , numbers : ( Int, Int )
-    , rndSolutions : List Int
-    , correctSolution : Int
-    , result : Answer
+    , operations : Nonempty Operation
+    , term : Term
+    , rndChoices : List Int
+    , answer : Answer
     }
 
 
 init : ( Model, Cmd Msg )
 init =
     ( { upper = 20
-      , operation = Add
-      , numbers = ( 1, 1 )
-      , rndSolutions = [ 1, 2, 3, 5 ]
-      , result = NotAnswered
-      , correctSolution = 2
+      , operations = Nel.cons Subtraction (Nel.fromElement Addition)
+      , term = { operation = Subtraction, first = 1, second = 1, result = 2 }
+      , rndChoices = [ 1, 2, 3, 4, 5 ]
+      , answer = NotAnswered
       }
-    , generateQuestion Add 20
+    , generateQuestion (Nel.fromElement Subtraction) 20
     )
 
 
@@ -54,80 +62,108 @@ init =
 ---- UPDATE ----
 
 
-generateQuestion : Operation -> Int -> Cmd Msg
-generateQuestion operation upper =
-    rndNumbers operation upper
+generateQuestion : Nonempty Operation -> Int -> Cmd Msg
+generateQuestion operations upper =
+    rndTerm operations upper
         |> Random.andThen
-            (\( nums, solution ) ->
-                rndSolutions operation upper nums |> Random.map (\xs -> ( nums, solution, xs ))
+            (\term ->
+                rndChoices upper term.result |> Random.map (\xs -> ( term, xs ))
             )
         |> Random.generate QuestionResult
 
 
-rndNumbers : Operation -> Int -> Random.Generator ( ( Int, Int ), Int )
-rndNumbers operation upper =
-    case operation of
-        Add ->
-            Random.int 0 upper
-                |> Random.andThen
-                    (\first ->
-                        Random.int 0 (upper - first)
-                            |> Random.map (\second -> ( ( first, second ), first + second ))
-                    )
+rndTerm : Nonempty Operation -> Int -> Random.Generator Term
+rndTerm operations upper =
+    Random.uniform (Nel.head operations) (Nel.tail operations)
+        |> Random.andThen
+            (\operation ->
+                case operation of
+                    Subtraction ->
+                        Random.int 0 upper
+                            |> Random.andThen
+                                (\first ->
+                                    Random.int 0 first
+                                        |> Random.map
+                                            (\second ->
+                                                { operation = operation
+                                                , first = first
+                                                , second = second
+                                                , result = first - second
+                                                }
+                                            )
+                                )
+
+                    Addition ->
+                        Random.int 0 upper
+                            |> Random.andThen
+                                (\first ->
+                                    Random.int 0 (upper - first)
+                                        |> Random.map
+                                            (\second ->
+                                                { operation = operation
+                                                , first = first
+                                                , second = second
+                                                , result = first + second
+                                                }
+                                            )
+                                )
+            )
 
 
-rndSolutions : Operation -> Int -> ( Int, Int ) -> Random.Generator (List Int)
-rndSolutions operation upper ( x, y ) =
-    case operation of
-        Add ->
-            Random.int 0 upper
-                |> Random.list 3
-                |> Random.map (List.append [ x + y ])
-                |> Random.andThen Random.List.shuffle
+rndChoices : Int -> Int -> Random.Generator (List Int)
+rndChoices upper correctAnswer =
+    Random.int 0 upper
+        |> Random.list 4
+        |> Random.map (List.append [ correctAnswer ])
+        |> Random.andThen Random.List.shuffle
 
 
 type Msg
-    = QuestionResult ( ( Int, Int ), Int, List Int )
+    = QuestionResult ( Term, List Int )
     | Next
     | TrySolve Int
+    | SelectOperation Operation
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        QuestionResult ( nums, solution, solutions ) ->
+        QuestionResult ( term, choices ) ->
             ( { model
-                | numbers = nums
-                , rndSolutions = solutions
-                , correctSolution = solution
-                , result = NotAnswered
+                | term = term
+                , rndChoices = choices
+                , answer = NotAnswered
               }
             , Cmd.none
             )
 
         Next ->
-            ( model, generateQuestion model.operation model.upper )
+            ( model, generateQuestion model.operations model.upper )
 
         TrySolve n ->
-            case model.operation of
-                Add ->
-                    if n == model.correctSolution then
-                        ( { model | result = Correct n }, Cmd.none )
+            if n == model.term.result then
+                ( { model | answer = Correct n }, Cmd.none )
 
-                    else
-                        ( { model | result = Incorrect n }, Cmd.none )
+            else
+                ( { model | answer = Incorrect n }, Cmd.none )
+
+        SelectOperation _ ->
+            ( model, Cmd.none )
 
 
 
 ---- VIEW ----
 
 
-viewTerm : Answer -> Operation -> ( Int, Int ) -> Html Msg
-viewTerm answer operation ( x, y ) =
+viewTerm : Answer -> Term -> Html Msg
+viewTerm answer { operation, first, second } =
     let
         operator =
             case operation of
-                Add ->
+                Subtraction ->
+                    " - "
+
+                Addition ->
                     " + "
 
         result =
@@ -141,16 +177,19 @@ viewTerm answer operation ( x, y ) =
                 NotAnswered ->
                     " _"
     in
-    Html.text (String.fromInt x ++ operator ++ String.fromInt y ++ " =" ++ result)
+    Html.text (String.fromInt first ++ operator ++ String.fromInt second ++ " =" ++ result)
 
 
-viewSolutions : List Int -> List (Html Msg)
-viewSolutions =
+viewChoices : List Int -> List (Html Msg)
+viewChoices =
     List.map
         (\x ->
             Button.button
-                [ Button.light
-                , Button.attrs [ Html.Attributes.style "font-size" "5vw" ]
+                [ Button.dark
+                , Button.attrs
+                    [ Html.Attributes.style "font-size" "5vw"
+                    , Html.Attributes.style "min-width" "10vw"
+                    ]
                 , Button.onClick (TrySolve x)
                 ]
                 [ Html.text (String.fromInt x) ]
@@ -166,17 +205,27 @@ view model =
                 [ Html.div
                     [ Html.Attributes.style "text-align" "center"
                     , Html.Attributes.style "font-size" "10vw"
+                    , Spacing.mt4
                     ]
-                    [ model.numbers |> viewTerm model.result model.operation ]
+                    [ viewTerm model.answer model.term ]
                 , Html.div
                     [ Html.Attributes.style "font-size" "5vw"
                     , Flex.block
                     , Flex.row
                     , Flex.justifyAround
                     ]
-                    (viewSolutions model.rndSolutions)
+                    (viewChoices model.rndChoices)
                 , Html.div [ Size.w100, Flex.block, Flex.col, Flex.alignItemsCenter, Spacing.mt3 ]
-                    [ Button.button [ Button.outlinePrimary, Button.onClick Next ] [ Html.text "Next" ]
+                    [ Button.button
+                        [ Button.light
+                        , Button.onClick Next
+                        , Button.attrs
+                            [ Spacing.mt3
+                            , Html.Attributes.style "font-size" "5vw"
+                            , Html.Attributes.style "min-width" "10vw"
+                            ]
+                        ]
+                        [ Html.i [ Html.Attributes.class "fas fa-step-forward" ] [] ]
                     , Button.linkButton
                         [ Button.roleLink, Button.attrs [ Spacing.mt3, Html.Attributes.href "https://github.com/battermann/kids-math-quiz" ] ]
                         [ Html.i [ Html.Attributes.class "fab fa-github" ] [], Html.text " Source Code" ]
